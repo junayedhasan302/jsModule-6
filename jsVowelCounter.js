e7f7a0612e73e3e49d13d943bb2027e88402a5c0
junayedhasan302 <junayedhasan302@gmail.com>
2026-07-23 03:11:07 +0600

    Vowel Counter, Vowel Checker, Minute to Second, Area of rect, Min Max, Avg, Divisibility Check, Ticket price

--- jsVowelCounter.js
+++ jsVowelCounter.js
@@ -1,5 +1,6 @@
 function vowelCounter(string){
-    let VowelCnt = 0, ConsonentCnt = 0;
+    let VowelCnt = 0,
+    ConsonentCnt = 0;
     for(char of string){
         if(char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u' || 
         char === 'A' || char === 'E' || char === 'I' || char === 'O' || char === 'U'){
