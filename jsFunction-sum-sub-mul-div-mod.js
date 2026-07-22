e999cbce799950d8b0910e81068bd431d862b03a
junayedhasan302 <junayedhasan302@gmail.com>
2026-07-22 23:42:17 +0600

    Sum-sub-mul-div-mod-square

---
+++ jsFunction-sum-sub-mul-div-mod.js
@@ -0,0 +1,24 @@
+function sum(a, b){
+    return a+b;
+}
+function sub(a, b){
+    return a-b;
+}
+function mul(a, b){
+    return a*b;
+}
+function div(a, b){
+    return a/b;
+}
+function mod(a, b){
+    return a%b;
+}
+function square(a){
+    return a*a;
+}
+console.log("Summation of two number: ", sum(10,20));
+console.log("Substraction of two number: ", sub(20,15));
+console.log("Multiplication of two number: ", mul(4,20));
+console.log("Division of two number: ", div(20,5));
+console.log("Remainder of a division: ", mod(15,4));
+console.log("Square of a numnber: ", square(4));
